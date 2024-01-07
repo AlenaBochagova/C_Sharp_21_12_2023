62cd20c8046d09a8b5a7c4a3ce80a9a972d833de
Alena <alenabochagova@mail.ru>
2024-01-07 15:24:02 +0300

    seminar 1 task 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два целых числа и проверяет, является ли первое число квадратом второго.
+
+Console.Write("Enter a number 1: ");
+int b1 = Convert.Tolnt32 (Console.ReadLine()!);
+Console.Write("Enter a number 2: ");
+int b2 = Convert.Tolnt32(Console.ReadLine()!);
+
+if (b2 *b2 == b1)
+    Console.WriteLine ("Yes");
+else
+    Console.WriteLine ("No");
